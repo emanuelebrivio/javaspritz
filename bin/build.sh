d2d37382e96b451a79cbec1efc94a882bb171a8c
Alessio Zappa <alessio@plasticpanda.com>
2015-05-27 19:53:55 +0200

    minify js files in cdn dir with livecompile

--- bin/build.sh
+++ bin/build.sh
@@ -2,3 +2,9 @@
 
 ./node_modules/.bin/jade $(pwd)/*.jade
 ./node_modules/.bin/stylus -c --use $(pwd)/node_modules/nib $(pwd)/static/css/style.styl
+
+JSFILES=$(pwd)/cdn/*.js
+for f in $JSFILES
+do
+  ./node_modules/.bin/uglifyjs $f -c -o ${f%.*}.min.js
+done
